3de7ff81fec6bb3cf8222a0979b49cfb45242871
pentagram5 <pentagram5@naver.com>
2024-12-29 16:54:08 +0900

    no-rebase

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log('helloww-hotfix');
+    console.log('helloww-hotfix!!');
     return 'Hello World!';
   }
 }
