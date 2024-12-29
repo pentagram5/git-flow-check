626a723c189e5f270792bce1c4e8d69f7cfa02da
pentagram5 <pentagram5@naver.com>
2024-12-29 16:00:02 +0900

    JWT-001 컨트롤러 업데이트
    # Conflicts:
    #	src/app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,13 +7,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    console.log('test1');
-    return this.appService.getHello();
-  }
-
-  @Post()
-  PostHello(): string {
-    console.log('test1');
     return this.appService.getHello();
   }
 }
